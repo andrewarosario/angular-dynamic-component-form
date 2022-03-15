be27149c0ea31f60562523ca8bb5b60bf2ce9ab1
andrewarosario <andrew.arosario@gmail.com>
2022-03-15 14:40:32 -0300

    create dynamic field Directive

---
+++ src/app/dynamic-form/dynamic-field.directive.ts
@@ -0,0 +1,26 @@
+import { Directive, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { DynamicFormConfig } from './dynamic-form-config.model';
+import { DynamicFormInputComponent } from './dynamic-form-input/dynamic-form-input.component';
+import { DynamicFormSelectComponent } from './dynamic-form-select/dynamic-form-select.component';
+
+const components = {
+  input: DynamicFormInputComponent,
+  select: DynamicFormSelectComponent,
+};
+
+@Directive({
+  selector: '[appDynamicField]',
+})
+export class DynamicFieldDirective<T> implements OnInit {
+  @Input() config: DynamicFormConfig;
+
+  constructor(private viewContainerRef: ViewContainerRef) {}
+
+  ngOnInit(): void {
+    const component = components[this.config.type];
+    const componentRef = this.viewContainerRef.createComponent(
+      component
+    ) as any;
+    componentRef.instance.config = this.config;
+  }
+}
--- src/app/dynamic-form/dynamic-form.module.ts
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormComponent } from './dynamic-form.component';
 import { DynamicFormInputComponent } from './dynamic-form-input/dynamic-form-input.component';
 import { DynamicFormSelectComponent } from './dynamic-form-select/dynamic-form-select.component';
+import { DynamicFieldDirective } from './dynamic-field.directive';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
   declarations: [
     DynamicFormComponent,
+    DynamicFieldDirective,
     DynamicFormInputComponent,
     DynamicFormSelectComponent,
   ],
