1e10a9c955d544822771bfdd8c8770126644aab9
andrewarosario <andrew.arosario@gmail.com>
2022-03-15 13:18:28 -0300

    create Dynamic Form Component

---
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.css'],
+})
+export class DynamicFormComponent implements OnInit {
+  @Input() formConfig = [];
+  form: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.formConfig.forEach((control) =>
+      this.form.addControl(control.name, new FormControl(''))
+    );
+  }
+}
--- src/app/dynamic-form/dynamic-form.module.ts
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormComponent } from './dynamic-form.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [],
+  declarations: [DynamicFormComponent],
+  exports: [DynamicFormComponent],
 })
 export class DynamicFormModule {}
