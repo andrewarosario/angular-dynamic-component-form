d5bd121e6c5375ec0c4961e7cf95d2a9161d519f
andrewarosario <andrew.arosario@gmail.com>
2022-03-15 13:33:56 -0300

    create pokémon trainer Service

---
+++ src/app/dynamic-form/dynamic-form-config.model.ts
@@ -0,0 +1,8 @@
+export type DynamicFormConfig = {
+  type: string;
+  label: string;
+  name: string;
+  initialValue?: any;
+  placeholder?: string;
+  options?: string[];
+};
---
+++ src/app/pokemon-trainer-form.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { DynamicFormConfig } from './dynamic-form/dynamic-form-config.model';
+
+@Injectable({ providedIn: 'root' })
+export class PokemonTrainerFormService {
+  getForm(): DynamicFormConfig[] {
+    return [
+      {
+        type: 'input',
+        label: 'Seu nome',
+        name: 'name',
+        placeholder: 'Digite seu nome, treinador Pokémon!',
+      },
+      {
+        type: 'input-number',
+        label: 'Quantos anos você tem',
+        name: 'age',
+      },
+      {
+        type: 'select',
+        label: 'Escolha seu Pokémon inicial',
+        name: 'initialPokemon',
+        options: ['Bulbasaur', 'Charmander', 'Squirtle'],
+      },
+    ];
+  }
+}
