9c2b5206de5ecdedc7e0052ab6ef798706cb306e
andrewarosario <andrew.arosario@gmail.com>
2022-03-15 13:39:56 -0300

    create dynamic form input

---
+++ src/app/dynamic-form/dynamic-form-input/dynamic-form-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ControlContainer, FormGroupDirective } from '@angular/forms';
+import { DynamicFormConfig } from '../dynamic-form-config.model';
+
+@Component({
+  selector: 'app-dynamic-form-input',
+  templateUrl: './dynamic-form-input.component.html',
+  styleUrls: ['./dynamic-form-input.component.css'],
+  viewProviders: [
+    { provide: ControlContainer, useExisting: FormGroupDirective },
+  ],
+})
+export class DynamicFormInputComponent {
+  config: DynamicFormConfig;
+}
--- src/app/dynamic-form/dynamic-form.module.ts
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicFormComponent } from './dynamic-form.component';
+import { DynamicFormInputComponent } from './dynamic-form-input/dynamic-form-input.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [DynamicFormComponent],
+  declarations: [DynamicFormComponent, DynamicFormInputComponent],
   exports: [DynamicFormComponent],
 })
 export class DynamicFormModule {}
